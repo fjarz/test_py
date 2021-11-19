73f3016a712cd2435747bb906e607a3574c44191
fjarz <94627763+fjarz@users.noreply.github.com>
2021-11-19 12:45:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hey you") 
+import csv
+
+import matplotlib.pyplot as plt
+
+filename = 'storm_data_search_results.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    injuries = []
+    for row in reader:
+        INJURIES_INDIRECT = int(row[19])
+        injuries.append(INJURIES_INDIRECT)
+    
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(injuries, c='red')
+
+plt.show()
+
